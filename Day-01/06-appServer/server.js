0f2febcf0cdf13c6483006f81d5a9aadc53a0437
Magesh <tkmagesh77@gmail.com>
2016-12-09 09:42:43 +0530

    appServer - done

--- Day-01/06-appServer/server.js
+++ Day-01/06-appServer/server.js
@@ -1,14 +1,18 @@
 var http = require('http'),
 	fs = require('fs'),
-	path = require('path');
+	path = require('path'),
+	url = require('url'),
+	querystring = require('querystring'),
+	calculator = require('./calculator');
 
 var staticExtns = ['.html', '.css', '.js', '.ico', '.png', '.xml', '.json'];
 function isStatic(resourceName){
 	return staticExtns.indexOf(path.extname(resourceName)) !== -1
 }
 var server = http.createServer(function(req, res){
-	console.log(req.url);
-	var resourceName = req.url === '/' ? '/index.html' : req.url;
+	var urlObj = url.parse(req.url);
+	var resourceName = urlObj.pathname === '/' ? '/index.html' : urlObj.pathname;
+	console.log(req.method + '\t' + urlObj.pathname);
 	if (isStatic(resourceName)){
 		var resourcePath = path.join(__dirname, resourceName);
 		fs.stat(resourcePath, function(err, stats){
@@ -25,9 +29,29 @@ var server = http.createServer(function(req, res){
 			var stream = fs.createReadStream(resourcePath);
 			stream.pipe(res);
 		});
-	} else if (req.url === '/calculator'){
-		res.write('calculator requests - coming soon!!');
+	} else if (resourceName === '/calculator' && req.method === 'GET'){
+		var queryData = querystring.parse(urlObj.query);
+		var op = queryData.op,
+			n1 = parseInt(queryData.n1, 10),
+			n2 = parseInt(queryData.n2, 10);
+		var result = calculator[op](n1, n2);
+		res.write(result.toString());
 		res.end();
+	} else if (resourceName === '/calculator' && req.method === 'POST'){
+		var inputData = '';
+		req.on('data', function(chunk){
+			inputData += chunk;
+		});
+		req.on('end', function(){
+			var bodyData = querystring.parse(inputData);
+			var op = bodyData.op,
+				n1 = parseInt(bodyData.n1, 10),
+				n2 = parseInt(bodyData.n2, 10);
+			var result = calculator[op](n1, n2);
+			res.write(result.toString());
+			res.end();
+		})
+		
 	} else {
 		res.statusCode = 404;
 		res.end();
